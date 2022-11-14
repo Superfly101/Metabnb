0020a0eb8718d0e9d18c93493ff8aaa79a2292d5
Daniel <danielukoha101@gmail.com>
2022-11-14 19:23:55 +0100

    chore: updating share modal styling

--- src/components/ConnectWallet/ConnectWallet.js
+++ src/components/ConnectWallet/ConnectWallet.js
@@ -11,9 +11,9 @@ const ConnectWallet = ({ onCloseHandler }) => {
     >
       {/* Modal */}
       <section className="bg-white py-4 rounded-xl w-full max-w-[30rem]">
-        <div className="relative mb-4 px-4">
-          <p className="">Connect Wallet</p>
-          <button className="absolute top-0 right-4" onClick={onCloseHandler}>
+        <div className="relative mb-4 px-6">
+          <p className="font-bold text-lg">Connect Wallet</p>
+          <button className="absolute top-0 right-6" onClick={onCloseHandler}>
             <svg width="16" height="16" viewBox="0 0 16 16">
               <path
                 d="M13.3536 3.35357L13.7072 3.00001L13.0001 2.29291L12.6465 2.64646L13.3536 3.35357ZM2.64652 12.6465L2.29297 13L3.00008 13.7071L3.35363 13.3536L2.64652 12.6465ZM3.35363 2.64646L3.00008 2.29291L2.29297 3.00001L2.64652 3.35357L3.35363 2.64646ZM12.6465 13.3536L13.0001 13.7071L13.7072 13L13.3536 12.6465L12.6465 13.3536ZM12.6465 2.64646L2.64652 12.6465L3.35363 13.3536L13.3536 3.35357L12.6465 2.64646ZM2.64652 3.35357L12.6465 13.3536L13.3536 12.6465L3.35363 2.64646L2.64652 3.35357Z"
@@ -23,15 +23,17 @@ const ConnectWallet = ({ onCloseHandler }) => {
           </button>
         </div>
 
-        <div className="py-2 px-4 border-t flex flex-col gap-3">
-          <p className="mt-2">Choose your preferred wallet:</p>
+        <div className="py-2 px-6 border-t flex flex-col gap-3">
+          <p className="mt-2 text-sm text-black">
+            Choose your preferred wallet:
+          </p>
           <div className="border relative rounded-lg p-3 flex gap-4 items-center cursor-pointer hover:bg-[#F8F9FA]">
             <img
               className="w-8"
               src="/assets/metamask-logo.png"
               alt="Metamask icon"
             />
-            <p>Metamask</p>
+            <p className="font-semibold">Metamask</p>
             <svg
               width="16"
               height="16"
@@ -48,7 +50,7 @@ const ConnectWallet = ({ onCloseHandler }) => {
               src="/assets/walletcoin-logo.png"
               alt="Metamask icon"
             />
-            <p>WalletCoin</p>
+            <p className="font-semibold">WalletCoin</p>
             <svg
               width="16"
               height="16"
